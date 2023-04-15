566e3f52b98832d4802e3a83de8443bdc7693e16
Justin Hornsby <justin.hornsby@entelect.co.za>
2023-04-15 12:10:35 +0200

    implement services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AccountsService } from './services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
+  accounts: {name: string, status: string}[] = [];
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    
-  }
-
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    
+  constructor(private accountsService: AccountsService) {}
+  
+  ngOnInit(): void {
+      this.accounts = this.accountsService.accounts;
   }
 }
